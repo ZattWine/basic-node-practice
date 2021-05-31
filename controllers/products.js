9f9e11fd75a20e1ad84dc35fdf74f96c5e3255ff
Zatt Wine <tomclick.org@gmail.com>
2021-05-31 23:32:55 +0630

    add controller sample implementations.

---
+++ controllers/products.js
@@ -0,0 +1,27 @@
+const products = [];
+
+exports.getAddProduct = (req, res, next) => {
+  res.render("add-product", {
+    pageTitle: "Add Product",
+    path: "/admin/add-product",
+    activeAddProduct: true,
+    productCSS: true,
+    formCSS: true,
+  });
+};
+
+exports.postAddProduct = (req, res, next) => {
+  products.push({ title: req.body.title });
+  res.redirect("/");
+};
+
+exports.getProducts = (req, res, next) => {
+  res.render("shop", {
+    data: products,
+    pageTitle: "Shop",
+    path: "/",
+    hasProducts: products.length > 0,
+    productCSS: true,
+    activeShop: true,
+  });
+};
--- routes/admin.js
+++ routes/admin.js
@@ -2,24 +2,12 @@ const express = require("express");
 
 const route = express.Router();
 
-const products = [];
+const productsController = require("../controllers/products");
 
 // /admin/add-product => GET
-route.get("/add-product", (req, res, next) => {
-  res.render("add-product", {
-    pageTitle: "Add Product",
-    path: "/admin/add-product",
-    activeAddProduct: true,
-    productCSS: true,
-    formCSS: true,
-  });
-});
+route.get("/add-product", productsController.getAddProduct);
 
 // /admin.add-product => POST
-route.post("/add-product", (req, res, next) => {
-  products.push({ title: req.body.title });
-  res.redirect("/");
-});
+route.post("/add-product", productsController.postAddProduct);
 
-exports.routes = route;
-exports.products = products;
+module.exports = route;
