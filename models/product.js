8c4f7141b0006066a568dc977fadfe7ed50c423f
Zatt Wine <tomclick.org@gmail.com>
2021-06-01 00:19:25 +0630

    Store data to file via the model.

--- models/product.js
+++ models/product.js
@@ -1,4 +1,5 @@
-const products = [];
+const fs = require("fs");
+const path = require("path");
 
 module.exports = class Product {
   constructor(t) {
@@ -6,10 +7,35 @@ module.exports = class Product {
   }
 
   save() {
-    products.push(this);
+    const p = path.join(
+      path.dirname(require.main.filename),
+      "data",
+      "products.json"
+    );
+
+    fs.readFile(p, (err, content) => {
+      let products = [];
+      if (!err) {
+        products = JSON.parse(content);
+      }
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        console.log(err);
+      });
+    });
   }
 
   static fetchAll() {
-    return products;
+    const p = path.join(
+      path.dirname(require.main.filename),
+      "data",
+      "products.json"
+    );
+
+    fs.readFile(p, (err, content) => {
+      if (err) return [];
+
+      return JSON.parse(content);
+    });
   }
 };
