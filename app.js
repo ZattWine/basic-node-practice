fa21f08b78751d7b6fd85efdb340702285606898
Zatt Wine <tomclick.org@gmail.com>
2021-05-31 21:40:55 +0630

    basic configuration of node application.

---
+++ app.js
@@ -0,0 +1,23 @@
+// Core
+const path = require("path");
+
+// Third party
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+const adminData = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/admin", adminData.routes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Page not found!</h1>");
+});
+
+app.listen(3000);
