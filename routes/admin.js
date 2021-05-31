7c63c8272bb13ba142d3e639545516b969508051
Zatt Wine <tomclick.org@gmail.com>
2021-06-01 01:34:03 +0630

    add product with complete information show all.

--- routes/admin.js
+++ routes/admin.js
@@ -2,12 +2,15 @@ const express = require("express");
 
 const route = express.Router();
 
-const productsController = require("../controllers/products");
+const adminController = require("../controllers/admin");
 
 // /admin/add-product => GET
-route.get("/add-product", productsController.getAddProduct);
+route.get("/add-product", adminController.getAddProduct);
+
+// /admin/products => GET
+route.get("/products", adminController.getProducts);
 
 // /admin.add-product => POST
-route.post("/add-product", productsController.postAddProduct);
+route.post("/add-product", adminController.postAddProduct);
 
 module.exports = route;
