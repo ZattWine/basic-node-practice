215b8e7ebc1715672c6f0cd5ce693f69e971f13a
Zatt Wine <tomclick.org@gmail.com>
2021-06-02 01:59:12 +0630

    add cart model and add one-to-many and many-to-many relations.

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const sequelize = require("./utils/database");
 
 const Product = require("./models/product");
 const User = require("./models/user");
+const Cart = require("./models/cart");
+const CartItem = require("./models/cart-item");
 
 const app = express();
 
@@ -37,12 +39,17 @@ app.use(shopRoutes);
 
 app.use(errorsController.get404Page);
 
+// Relationships or Associations
 Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
 
 sequelize
-  // .sync({ force: true })
-  .sync()
+  .sync({ force: true })
+  // .sync()
   .then((result) => {
     return User.findByPk(1);
   })
