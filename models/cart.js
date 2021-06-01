b90c9c8b7340628b96f558438f5952c805e8fb4f
Zatt Wine <tomclick.org@gmail.com>
2021-06-01 19:39:51 +0630

    adding a cart model.

---
+++ models/cart.js
@@ -0,0 +1,42 @@
+const fs = require("fs");
+const path = require("path");
+
+const p = path.join(path.dirname(require.main.filename), "data", "cart.json");
+
+module.exports = class Cart {
+  static addProduct(id, productPrice) {
+    // fetch the previous cart
+    fs.readFile(p, (err, content) => {
+      // init cart
+      let cart = { products: [], totalPrice: 0 };
+
+      if (!err) {
+        cart = JSON.parse(content);
+      }
+
+      // Analyze the cart => find existing product
+      const existingProductIndex = cart.products.findIndex(
+        (item) => item.id === id
+      );
+      const existingProduct = cart.products[existingProductIndex];
+      let updatedProduct;
+
+      // Add new product and increase quantity
+      if (existingProduct) {
+        updatedProduct = { ...existingProduct };
+        updatedProduct.qty = updatedProduct.qty + 1;
+
+        cart.products = [...cart.products];
+        cart.products[existingProductIndex] = updatedProduct;
+      } else {
+        updatedProduct = { id: id, qty: 1 };
+        cart.products = [...cart.products, updatedProduct];
+      }
+
+      cart.totalPrice = cart.totalPrice + +productPrice;
+      fs.writeFile(p, JSON.stringify(cart), (err) => {
+        console.log(err);
+      });
+    });
+  }
+};
