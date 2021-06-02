d5727e09e9da4d2465759a600c84a71f8b90fe9e
Zatt Wine <tomclick.org@gmail.com>
2021-06-03 01:26:13 +0630

    db: using relations in mongoose

--- controllers/admin.js
+++ controllers/admin.js
@@ -18,6 +18,7 @@ exports.postAddProduct = (req, res, next) => {
     price: price,
     description: description,
     imageUrl: imageUrl,
+    userId: req.user, // mongoose will check _id from User
   });
   product
     .save()
