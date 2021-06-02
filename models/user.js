1c61b4a407a61b5ae6755a1592b55386dbe0189c
Zatt Wine <tomclick.org@gmail.com>
2021-06-02 21:31:58 +0630

    db: add relation order from cart.

--- models/user.js
+++ models/user.js
@@ -81,9 +81,18 @@ class User {
 
   addOrder() {
     const db = getDb();
-    return db
-      .collection("orders")
-      .insertOne(this.cart)
+    return this.getCart()
+      .then((products) => {
+        const order = {
+          items: products,
+          user: {
+            _id: ObjectId(this._id),
+            name: this.name,
+            email: this.email,
+          },
+        };
+        return db.collection("orders").insertOne(order);
+      })
       .then((result) => {
         this.cart = { items: [] };
         return db
