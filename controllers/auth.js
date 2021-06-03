00c487aa5f5db09062c4d4829a2f9995a644eaa8
Zatt Wine <tomclick.org@gmail.com>
2021-06-03 18:23:09 +0630

    show error message in login page

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,10 +3,16 @@ const bcrypt = require("bcryptjs");
 const User = require("../models/user");
 
 exports.getLogin = (req, res, next) => {
-  console.log(req.session.isLoggedIn);
+  let message = req.flash("error");
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
   res.render("auth/login", {
     path: "/login",
     pageTitle: "Login",
+    errorMessage: message,
   });
 };
 
@@ -23,6 +29,7 @@ exports.postLogin = (req, res, next) => {
   User.findOne({ email: email })
     .then((user) => {
       if (!user) {
+        req.flash("error", "Invalid email or password!");
         return res.redirect("/login");
       }
 
