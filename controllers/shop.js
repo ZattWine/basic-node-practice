dfb017db62500d19bb16ac2be8715209f8c2bea4
Zatt Wine <tomclick.org@gmail.com>
2021-06-04 19:53:29 +0630

    using pdfkit for pdf generation.

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,6 +1,8 @@
 const fs = require("fs");
 const path = require("path");
 
+const PDFDocument = require('pdfkit')
+
 const Product = require("../models/product");
 const Order = require("../models/order");
 
@@ -162,6 +164,16 @@ exports.getInvoice = (req, res, next) => {
       const invoiceName = "invoice-" + orderId + ".pdf";
       const invoicePath = path.join("data", "invoices", invoiceName);
 
+      const pdfDoc = new PDFDocument();
+      res.setHeader("Content-Type", "application/pdf");
+      res.setHeader("Content-Disposition", `inline; filename="${invoiceName}"`);
+      pdfDoc.pipe(fs.createWriteStream(invoicePath));
+      pdfDoc.pipe(res);
+
+      pdfDoc.text('Hello World!');
+
+      pdfDoc.end();
+
       // preloading data method
       // fs.readFile(invoicePath, (err, data) => {
       //   if (err) {
@@ -177,10 +189,10 @@ exports.getInvoice = (req, res, next) => {
       // });
 
       // streaming data method
-      const file = fs.createReadStream(invoicePath);
-      res.setHeader("Content-Type", "application/pdf");
-      res.setHeader("Content-Disposition", `inline; filename="${invoiceName}"`);
-      file.pipe(res);
+      // const file = fs.createReadStream(invoicePath);
+      // res.setHeader("Content-Type", "application/pdf");
+      // res.setHeader("Content-Disposition", `inline; filename="${invoiceName}"`);
+      // file.pipe(res);
     })
     .catch((err) => next(err));
 };
