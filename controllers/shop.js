4b35a7362a13d5420ad3c4f979a27581a23a4a30
Zatt Wine <tomclick.org@gmail.com>
2021-06-05 13:43:10 +0630

    complete add payment method using stripe

--- controllers/shop.js
+++ controllers/shop.js
@@ -2,6 +2,8 @@ const fs = require("fs");
 const path = require("path");
 
 const PDFDocument = require("pdfkit");
+const { STRIPE_KEY } = require("../utils/consts");
+const stripe = require("stripe")(STRIPE_KEY);
 
 const Product = require("../models/product");
 const Order = require("../models/order");
@@ -139,23 +141,71 @@ exports.postCartDelete = (req, res, next) => {
 };
 
 exports.getCheckout = (req, res, next) => {
+  let products;
+  let total = 0;
   req.user
     .populate("cart.items.productId")
     .execPopulate()
     .then((user) => {
-      const products = user.cart.items;
-      let total = 0;
+      products = user.cart.items;
+      total = 0;
 
-      products.forEach(p => {
+      products.forEach((p) => {
         total += p.quantity * p.productId.price;
-      })
+      });
 
+      return stripe.checkout.sessions.create({
+        payment_method_types: ["card"],
+        line_items: products.map((p) => {
+          return {
+            name: p.productId.title,
+            description: p.productId.description,
+            amount: p.productId.price * 100,
+            currency: "usd",
+            quantity: p.quantity,
+          };
+        }),
+        success_url: `${req.protocol}://${req.get("host")}/checkout/success`, // http://localhost:3000
+        cancel_url: `${req.protocol}://${req.get("host")}/checkout/cancel`,
+      });
+    })
+    .then((session) => {
+      // console.log(session);
       res.render("shop/checkout", {
         path: "/checkout",
         pageTitle: "Checkout",
         products: products,
         totalSum: total,
+        sessionId: session.id,
       });
+    })
+    .catch((err) => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
+    });
+};
+
+exports.getCheckoutSuccess = (req, res, next) => {
+  req.user
+    .populate("cart.items.productId")
+    .execPopulate() // bcoz there is no Promise return in populate
+    .then((user) => {
+      const products = user.cart.items.map((i) => {
+        return { quantity: i.quantity, product: { ...i.productId._doc } };
+      });
+      const order = new Order({
+        user: {
+          name: req.user.email,
+          userId: req.user,
+        },
+        products: products,
+      });
+      return order.save();
+    })
+    .then((result) => {
+      req.user.clearCart();
+      res.redirect("/orders");
     })
     .catch((err) => {
       const error = new Error(err);
